e5e9946f245cd879a6f317e7b3829cae7dc5223d
filipetaremba <filipetaremba0@gmail.com>
2026-05-18 00:01:01 +0200

    Inicio 18/5

--- mobile-app/babel.config.js
+++ mobile-app/babel.config.js
@@ -1,7 +1,13 @@
 module.exports = function (api) {
   api.cache(true);
+
   return {
-    presets: [["babel-preset-expo", { jsxImportSource: "nativewind" }]],
-    plugins: ["nativewind/babel"],
+    presets: [
+      "babel-preset-expo",
+      "nativewind/babel"
+    ],
+    plugins: [
+      "expo-router/babel"
+    ],
   };
 };
